cf37dcdf7bff934b9517062793230ceb908b19a5
LexyPC <alexipre89@hotmail.com>
2014-10-19 15:16:16 -0500

    tercer avance del proyecto

---
+++ Vistas/menu.php
@@ -0,0 +1,7 @@
+<?php
+	echo '<br><a href="index.php?controlador=vehiculos&accion=altavehiculo">Alta</a>';
+	echo '<br><a href="index.php?controlador=vehiculos&accion=darbaja">Baja</a>';
+	echo '<br><a href="index.php?controlador=vehiculos&accion=vista">Vista</a>';
+	echo '<br><a href="index.php?controlador=vehiculos&accion=modificarubicacion">Modifica Ubicacion</a>';
+	echo '<br><a href="index.php?controlador=usuarios&accion=cerrar">Cerrar sesion</a>';
+?>
