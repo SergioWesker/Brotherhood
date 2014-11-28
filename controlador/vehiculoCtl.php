b02842a0c8a924da048e41efe22bed8a731ea255
SergioWesker <wesker06.91@gmail.com>
2014-11-27 22:05:23 -0600

    Proyecto final

--- controlador/vehiculoCtl.php
+++ controlador/vehiculoCtl.php
@@ -9,6 +9,7 @@
 		public $Ubicacion;
 		public $cierra;
 		public $abre;
+		public $filas;
 		
 		public function __construct()
 		{
@@ -29,13 +30,24 @@
 			{
 				case 'altavehiculo':
 					$validar = $this->controlador->isAdmin();
-					if ($validar==false){
-						echo "Debes ser administrador para poder ingresar un vehiculo";
-						require("Vistas/menu.php");
+					$validar2 = $this->controlador->isWorker();
+					if ($validar==false&&$validar2==false){
+						echo '<div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+   Debes ser administrador o trabajador</div>';
+						require("Vistas/header.html");
 					}
 					else{
 						if(empty($_POST))
+						{
+							$vista = file_get_contents("Vistas/ModificarEliminar.html");
+							$header = file_get_contents("Vistas/cabecera.html");
+							echo $header;
+							echo $vista;
 							require("Vistas/AltaVehiculos.php");
+							$footer = file_get_contents("Vistas/pie.html");
+							echo $footer;
+						}
 						else{
 							//Se obtienen las variables para alta y se limpian
 							if(isset($_POST["VIN"]) && is_numeric($_POST["VIN"]))
@@ -107,28 +119,36 @@
 								$vehiculo = $this->modelo->alta($VIN,$Marca,$Modelo,$Color,$Caracteristicas);
 								$inv = $this->modelo->inventario($VIN,$Kilometraje,$Combustible,$Golpes);
 								$ubi = $this->modelo->ubicacion($VIN,$Ubicacion);
-								echo "Vehiculo registrado";
+								echo '<div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+   Vehiculo registrado</div>';
 							}
 						
 						}
 					}
 					break;
-				case "vista":
-					$validar = $this->controlador->isAdmin();
-					$validarU = $this->controlador->isUser();
 					
-					if ($validar==false&&$validarU==false) {
-						echo "Necesitas ser usuario o administrador para ver los vehiculos registrados";
-						require("Vistas/menu.html");
+					
+				case "listado":
+					$validar = $this->controlador->isAdmin();
+					
+					if ($validar==false) {
+						echo '<div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+   Debes ser administrador</div>';
+						require("Vistas/header.html");
 					}
 					else{
 						$listar = $this ->modelo -> lista();
-						$vista = file_get_contents("Vistas/ListadoVehiculos.html");
+						$vista = file_get_contents("Vistas/Web/tabla.html");
+						$header = file_get_contents("Vistas/cabecera.html");
 						$footer = file_get_contents("Vistas/pie.html");
 						//Obtengo la fila de la tabla
 						$inicio_fila = strrpos($vista,'<tr>');
 						$final_fila = strrpos($vista,'</tr>') + 5;
 						$fila = substr($vista,$inicio_fila,$final_fila-$inicio_fila);
+
+						$filas =null;
 
 						foreach ($listar as $dato){
 							$new_fila = $fila;
@@ -138,28 +158,88 @@
 							'{Marca}' => $dato["marca"],
 							'{Modelo}' => $dato["modelo"],
 							'{Color}' => $dato["color"],
-							'{Caracteristicas}' => $dato["caracteristicas"],
-                                                        '{Ubicacion}' => $dato["Ubicacion"]);
+							'{Caracteristicas}' => $dato["caracteristicas"]);
 							$new_fila = strtr($new_fila,$diccionario);
+
 							$filas .= $new_fila;
 						}
 						$vista = str_replace($fila, $filas, $vista);
 						$header = strtr($header,$diccionario);
 						$vista = $header . $vista . $footer;
 						//Mostrar la vista
-						echo $vista;
+					echo $vista;
 					}
 					break;
 					
+					
+				case "vehiculosusuario":
+				$validar = $this->controlador->isAdmin();
+				$validarU = $this->controlador->isUser();
+
+				if ($validar==false&&$validarU==false) {
+
+					echo '<div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+   Debes ser administrador o usuario</div>';
+					require("Vistas/header.html");
+				}
+				else{
+					$usuario = $_SESSION['user'];
+					$listar = $this ->modelo -> listaU($usuario);
+					if($listar==null)
+					{echo '<div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+   Este usuario no tiene vehiculos</div>';}
+					else {
+						$vista = file_get_contents("Vistas/ModificarEliminar.html");
+						$header = file_get_contents("Vistas/cabecera.html");
+						$footer = file_get_contents("Vistas/pie.html");
+						//Obtengo la fila de la tabla
+						$inicio_fila = strrpos($vista, '<tr>');
+						$final_fila = strrpos($vista, '</tr>') + 5;
+						$fila = substr($vista, $inicio_fila, $final_fila - $inicio_fila);
+
+
+						foreach ($listar as $dato) {
+							$new_fila = $fila;
+							//Reemplazo con un diccionario
+							$diccionario = array(
+								'{VIN}' => $dato["VIN"],
+								'{Marca}' => $dato["marca"],
+								'{Modelo}' => $dato["modelo"],
+								'{Color}' => $dato["color"],
+								'{Caracteristicas}' => $dato["caracteristicas"]);
+							$new_fila = strtr($new_fila, $diccionario);
+							$filas .= $new_fila;
+						}
+						$vista = str_replace($fila, $filas, $vista);
+						$header = strtr($header, $diccionario);
+						$vista = $header . $vista . $footer;
+						//Mostrar la vista
+						echo $vista;
+					}
+				}
+				break;
+
 				case 'modificarubicacion':
 					$validar = $this->controlador->isAdmin();
-					if ($validar==false) {
-						echo "Debes ser administrador para poder modificar la ubicacion de un vehiculo";
-						require("Vistas/menu.php");
+					$validar2 = $this->controlador->isWorker();
+					if ($validar==false&&$validar2==false) {
+						echo '<div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+   Debes ser administrador o trabajador</div>';
+						require("Vistas/header.html");
 					}
 					else{
-						if(empty($_POST))
-							require("Vistas/CambioUbicacion.php");
+						if(empty($_POST)) {
+							$vista = file_get_contents("Vistas/ModificarEliminar.html");
+							$header = file_get_contents("Vistas/cabecera.html");
+							echo $header;
+							echo $vista;
+							require("Vistas/Cambioubicacion.php");
+							$footer = file_get_contents("Vistas/pie.html");
+							echo $footer;
+						}
 						else{
 							//Se obtienen las variables para alta y se limpian
 							if(isset($_POST["VIN"]) && is_numeric($_POST["VIN"]))
@@ -195,6 +275,9 @@
 								require("Vistas/error.html");
 							else{
 								$modificar = $this->modelo->modificarubicacion($VIN,$Nombre,$Causa,$NUbicacion);
+								echo '<div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+   Ubicacion modificada</div>';
 							}
 
 						}	
@@ -203,13 +286,23 @@
 							
 				case 'darbaja':
 					$validar = $this->controlador->isAdmin();
-					if ($validar==false) {
-						echo "Debes ser administrador para poder dar de baja un vehiculo";
-						require("Vistas/menu.php");
+					$validar2 = $this->controlador->isWorker();
+					if ($validar==false&&$validar2==false) {
+						echo '<div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+   Debes ser administrador o trabajador</div>';
+						require("Vistas/header.html");
 					}
 					else{
-						if(empty($_POST))
+						if(empty($_POST)) {
+							$vista = file_get_contents("Vistas/ModificarEliminar.html");
+							$header = file_get_contents("Vistas/cabecera.html");
+							echo $header;
+							echo $vista;
 							require("Vistas/BajaVehiculos.php");
+							$footer = file_get_contents("Vistas/pie.html");
+							echo $footer;
+						}
 						else{
 							if(isset($_POST["VIN"]) && is_numeric($_POST["VIN"]))
 								$VIN	= $_POST["VIN"];		
@@ -227,12 +320,103 @@
 
 							if (($VIN=="")||($Estado=="")) 
 								require("Vistas/error.html");
-							else {
+							else 
 								$darbaja = $this->modelo->baja($VIN,$Estado);
-							}
+							echo '<div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+   Vehiculo terminado</div>';
 						}
 					}
 					break;
+				case 'modificarinventario':
+					$validar = $this->controlador->isAdmin();
+					if ($validar==false) {
+						echo '<div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+   Debes ser administrador o trabajador</div>';
+						require("Vistas/header.html");
+					}
+					else{
+						if(empty($_POST)){
+							$vista = file_get_contents("Vistas/ModificarEliminar.html");
+							$header = file_get_contents("Vistas/cabecera.html");
+							echo $header;
+							echo $vista;
+							require("Vistas/modificarinventario.php");
+							$footer = file_get_contents("Vistas/pie.html");
+							echo $footer;
+						}
+						else{
+							//Se obtienen las variables para alta y se limpian
+							if(isset($_POST["VIN"]) && is_numeric($_POST["VIN"]))
+								$VIN	= $_POST["VIN"];
+							else{
+								$VIN = "";
+								echo "Identificador no válido<br>";
+							}
+
+							if(isset($_POST["kilometraje"]) && is_numeric($_POST["kilometraje"]))
+								$Kilometraje = $_POST["kilometraje"];
+							else{
+								$Kilometraje = "";
+								echo "El kilometraje debe ser numérico<br>";
+							}
+
+							if(isset($_POST["combustible"]) && is_float((float) $_POST["combustible"]))
+								$Combustible = $_POST["combustible"];
+							else{
+								$Combustible = "";
+								echo "Error la cantidad debe ser flotante<br>";
+							}
+							if(isset($_POST["golpes"]) && preg_match($regex, $_POST["golpes"]))
+								$Golpes = $_POST["golpes"];
+							else{
+								$Golpes = "";
+								echo "Error<br>";
+							}
+							$inventario = $this->modelo->modificarinventario($VIN,$Kilometraje,$Combustible,$Golpes);
+							echo '<div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+   Inventario modificado</div>';
+						}
+					}
+					break;
+				case 'eliminarinventario':
+					$validar = $this->controlador->isAdmin();
+					if ($validar==false) {
+						echo '<div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+   Debes ser administrador o trabajador</div>';
+						require("Vistas/header.html");
+					}
+					else{
+						if(empty($_POST)){
+							$vista = file_get_contents("Vistas/ModificarEliminar.html");
+							$header = file_get_contents("Vistas/cabecera.html");
+							echo $header;
+							echo $vista;
+							require("Vistas/eliminarinventario.php");
+							$footer = file_get_contents("Vistas/pie.html");
+							echo $footer;
+						}
+						else{
+							//Se obtienen las variables para alta y se limpian
+							if(isset($_POST["VIN"]) && is_numeric($_POST["VIN"]))
+								$VIN	= $_POST["VIN"];
+							else{
+								$VIN = "";
+								echo "Identificador no válido<br>";
+							}
+
+
+							$eliminar_inv = $this->modelo->eliminarinventario($VIN);
+							echo '<div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+   Inventario eliminado</div>';
+						}
+					}
+					break;
+
 			}
 		}
 	}
