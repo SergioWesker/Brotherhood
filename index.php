9b64503e2375602ed43a23941eb69589fbbe4e4f
SergioWesker <wesker06.91@gmail.com>
2014-11-27 22:03:24 -0600

    Proyecto final

--- index.php
+++ index.php
@@ -1,20 +1,32 @@
 <?php
 	session_start();
-	switch($_REQUEST['controlador'])
-	{
-		case 'vehiculos':
-			//Carga el controlador
-			require('controlador/vehiculoCtl.php');
-			$controlador = new VehiculoCtl();
-			
-			//Ejecuta el controlador
-			$controlador->ejecutar();
-			break;
+	$controlador = null;
+	if (empty($_GET) || empty($_REQUEST['controlador'])) {
+		require("Vistas/iniciar.html");
+	}
+	else {
+		switch($_REQUEST['controlador'])
+		{
+			case 'vehiculos':
+				//Carga el controlador
+				require('controlador/vehiculoCtl.php');
+				$controlador = new VehiculoCtl();
 
-		case 'usuarios':
-			require('controlador/sesionCtl.php');
-			$controlador = new SesionCtl();
-			$controlador->isLogged();
-			break;
+				//Ejecuta el controlador
+				$controlador->ejecutar();
+				break;
+
+			case 'usuarios':
+				require('controlador/sesionCtl.php');
+				$controlador = new SesionCtl();
+				$controlador->isLogged();
+				break;
+
+			case 'registro':
+				require('controlador/usuarioCtl.php');
+				$controlador = new UsuarioCtl();
+				$controlador->ejecutar();
+				break;
+		}
 	}
 ?>
